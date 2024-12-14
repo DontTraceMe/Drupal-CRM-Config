3848d5f0a77011a69f019967bb3fdc443295c61e
DontTraceMe <megajaunt15@gmail.com>
2024-12-14 17:57:25 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	$servername = "localhost";
+	$username = "dc7user";
+	$password = "belfastAdmin!!";
+	$dbname = "Staff";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
